cc294469d3eef691cbabd0b52a8ec1af0fdd60ee
天马行空 <8036004+hruyi@user.noreply.gitee.com>
2025-01-27 21:48:32 +0800

    7.9 封装分页返回结果

--- member/src/main/java/com/hry/train/member/controller/PassengerController.java
+++ member/src/main/java/com/hry/train/member/controller/PassengerController.java
@@ -1,14 +1,15 @@
 package com.hry.train.member.controller;
 
+import com.hry.train.common.context.LoginMemberContext;
 import com.hry.train.common.resp.CommonResp;
+import com.hry.train.common.resp.PageResp;
+import com.hry.train.member.req.PassengerQueryReq;
 import com.hry.train.member.req.PassengerSaveReq;
+import com.hry.train.member.resp.PassengerQueryResp;
 import com.hry.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/passenger")
@@ -20,5 +21,11 @@ public class PassengerController {
     public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req) {
         passengerService.save(req);
         return new CommonResp<>();
+    }
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req) {
+        req.setMemberId(LoginMemberContext.getId());
+        PageResp<PassengerQueryResp> list = passengerService.queryList(req);
+        return new CommonResp<>(list);
     }
 }
--- member/src/main/java/com/hry/train/member/service/PassengerService.java
+++ member/src/main/java/com/hry/train/member/service/PassengerService.java
@@ -2,16 +2,28 @@ package com.hry.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.hry.train.common.context.LoginMemberContext;
+import com.hry.train.common.resp.PageResp;
 import com.hry.train.common.util.SnowUtil;
 import com.hry.train.member.domain.Passenger;
+import com.hry.train.member.domain.PassengerExample;
 import com.hry.train.member.mapper.PassengerMapper;
+import com.hry.train.member.req.PassengerQueryReq;
 import com.hry.train.member.req.PassengerSaveReq;
+import com.hry.train.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
+    private static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
     @Resource
     private PassengerMapper passengerMapper;
     public void save(PassengerSaveReq req) {
@@ -22,5 +34,29 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+    public PageResp<PassengerQueryResp> queryList(PassengerQueryReq req) {
+        PassengerExample passengerExample = new PassengerExample();
+        passengerExample.setOrderByClause("id desc");
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        if (ObjectUtil.isNotNull(req.getMemberId())) {
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
+
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+
+        PageInfo<Passenger> pageInfo = new PageInfo<>(passengerList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        List<PassengerQueryResp> list = BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
+
+        PageResp<PassengerQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
     }
 }
