0f29c73b9545713f13f9e47c0e119cd7bc79789a
天马行空 <8036004+hruyi@user.noreply.gitee.com>
2025-01-30 17:03:29 +0800

    9.16 实现按车次生成车座功能，增加后端接口

--- business/src/main/java/com/hry/train/business/config/BusinessApplication.java
+++ business/src/main/java/com/hry/train/business/config/BusinessApplication.java
@@ -1,5 +1,6 @@
 package com.hry.train.business.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,7 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.hry")
-//@MapperScan("com.hry.train.*.mapper")
+@MapperScan("com.hry.train.*.mapper")
 public class BusinessApplication {
     private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
 
