3d5ce48fb59564e01cbbc18610733d5315b2e10a
天马行空 <8036004+hruyi@user.noreply.gitee.com>
2025-02-01 14:42:15 +0800

    10.6 禁止任务并发执行

--- batch/src/main/java/com/hry/train/batch/job/TestJob.java
+++ batch/src/main/java/com/hry/train/batch/job/TestJob.java
@@ -1,13 +1,20 @@
 package com.hry.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("Test..........JOB");
+        System.out.println("TestJob Test开始");
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("TestJob Test结束");
     }
 }
