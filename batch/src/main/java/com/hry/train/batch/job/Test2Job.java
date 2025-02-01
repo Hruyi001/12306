1c9e5babc91e648b8e1505bdd95817335f61fead
天马行空 <8036004+hruyi@user.noreply.gitee.com>
2025-02-01 15:50:32 +0800

    10.10 多节点定时任务，轮询执行

--- batch/src/main/java/com/hry/train/batch/job/Test2Job.java
+++ batch/src/main/java/com/hry/train/batch/job/Test2Job.java
@@ -4,17 +4,18 @@ import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
 @DisallowConcurrentExecution
-public class TestJob implements Job {
+public class Test2Job implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob Test开始");
+        System.out.println("TestJob2222222222222222 Test开始");
 //        try {
 //            Thread.sleep(3000);
 //        } catch (Exception e) {
 //            throw new RuntimeException(e);
 //        }
-        System.out.println("TestJob Test结束");
+        System.out.println("TestJob22222222222222222 Test结束");
     }
 }
