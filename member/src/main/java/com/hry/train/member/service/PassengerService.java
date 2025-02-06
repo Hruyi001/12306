04fc000181a453ab049cb5fe170ccd29ed15aa62
天马行空 <8036004+hruyi@user.noreply.gitee.com>
2025-02-06 16:38:51 +0800

    13.7 集成Nacos注册中心，所有的SpringBoot模块都需要连接注册中心，common中引入nacos

--- member/src/main/java/com/hry/train/member/service/PassengerService.java
+++ member/src/main/java/com/hry/train/member/service/PassengerService.java
@@ -8,8 +8,12 @@ import com.github.pagehelper.PageInfo;
 import com.hry.train.common.context.LoginMemberContext;
 import com.hry.train.common.resp.PageResp;
 import com.hry.train.common.util.SnowUtil;
+import com.hry.train.member.domain.Member;
+import com.hry.train.member.domain.MemberExample;
 import com.hry.train.member.domain.Passenger;
 import com.hry.train.member.domain.PassengerExample;
+import com.hry.train.member.enums.PassengerTypeEnum;
+import com.hry.train.member.mapper.MemberMapper;
 import com.hry.train.member.mapper.PassengerMapper;
 import com.hry.train.member.req.PassengerQueryReq;
 import com.hry.train.member.req.PassengerSaveReq;
@@ -19,6 +23,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -26,6 +32,9 @@ public class PassengerService {
     private static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
     @Resource
     private PassengerMapper passengerMapper;
+
+    @Resource
+    private MemberMapper memberMapper;
     public void save(PassengerSaveReq req) {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
@@ -68,5 +77,49 @@ public class PassengerService {
 
     public void delete(Long id) {
         passengerMapper.deleteByPrimaryKey(id);
+    }
+    public List<PassengerQueryResp> queryMine() {
+        PassengerExample passengerExample = new PassengerExample();
+        passengerExample.setOrderByClause("name asc");
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        criteria.andMemberIdEqualTo(LoginMemberContext.getId());
+        List<Passenger> list = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(list, PassengerQueryResp.class);
+    }
+
+    /**
+     * 初始化乘客，如果没有张三，就增加乘客张三，李四、王五同理，防止线上体验时乘客被删光
+     */
+    public void init() {
+        DateTime now = DateTime.now();
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo("13000000000");
+
+        List<Member> memberList = memberMapper.selectByExample(memberExample);
+        Member member = memberList.get(0);
+
+        List<Passenger> passengerList = new ArrayList<>();
+
+        List<String> nameList = Arrays.asList("张三", "李四", "王五");
+        for (String s : nameList) {
+            Passenger passenger = new Passenger();
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setMemberId(member.getId());
+            passenger.setName(s);
+            passenger.setIdCard("123456789123456789");
+            passenger.setType(PassengerTypeEnum.ADULT.getCode());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerList.add(passenger);
+        }
+
+        for (Passenger passenger : passengerList) {
+            PassengerExample passengerExample = new PassengerExample();
+            passengerExample.createCriteria().andNameEqualTo(passenger.getName());
+            long l = passengerMapper.countByExample(passengerExample);
+            if (l == 0) {
+                passengerMapper.insert(passenger);
+            }
+        }
     }
 }
